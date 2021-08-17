4d29f43815030d3f03f0f57ed3ae60111a5fc300
Girish Gopaul <ggopaul0510@gmail.com>
2021-08-17 14:06:56 +0400

    Minor refactor

--- lib/employees_api_web/router.ex
+++ lib/employees_api_web/router.ex
@@ -11,12 +11,14 @@ defmodule EmployeesApiWeb.Router do
     resources "/job_titles", JobTitleController, except: [:new, :edit]
 
     resources "/employees", EmployeeController, except: [:new, :edit]
+
     scope "/employees" do
       put "/:id/picture", EmployeeController, :upload_picture
       delete "/:id/picture", EmployeeController, :delete_picture
     end
 
     resources "/users", UserController, only: [:create]
+
     scope "/users" do
       post "/login", UserController, :login
     end
