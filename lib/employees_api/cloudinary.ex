734b7ab29cff73f0cb721ff4af05f9f839c5d150
Girish Gopaul <girish@gopaul.me>
2023-04-16 14:53:06 +0400

    changed cloudinary env var

--- lib/employees_api/cloudinary.ex
+++ lib/employees_api/cloudinary.ex
@@ -1,9 +1,10 @@
 defmodule EmployeesApi.Cloudinary do
   @endpoint "https://api.cloudinary.com/v1_1"
+  @cloudinary_url URI.parse(System.fetch_env!("CLOUDINARY_URL"))
 
   def upload(params) do
     params
-    |> send_put
+    |> send_put()
     |> Map.fetch!(:body)
     |> Jason.decode!()
   end
@@ -11,7 +12,7 @@ defmodule EmployeesApi.Cloudinary do
   def destroy(public_id) do
     HTTPoison.post!(
       "#{endpoint()}/image/destroy",
-      %{"public_id" => public_id} |> build_body |> Jason.encode!(),
+      %{"public_id" => public_id} |> build_body() |> Jason.encode!(),
       "Content-Type": "application/json"
     )
   end
@@ -24,8 +25,8 @@ defmodule EmployeesApi.Cloudinary do
     )
   end
 
-  defp endpoint do
-    @endpoint <> "/" <> System.fetch_env!("CLOUDINARY_CLOUD_NAME")
+  defp endpoint() do
+    @endpoint <> "/" <> elem(parse_cloudinary_url(), 2)
   end
 
   defp build_multipart(%{filename: filename, path: path, content_type: content_type}) do
@@ -47,7 +48,7 @@ defmodule EmployeesApi.Cloudinary do
 
   defp build_body(additional_params \\ %{}) do
     timestamp = DateTime.utc_now() |> DateTime.to_unix()
-    secret = System.fetch_env!("CLOUDINARY_SECRET")
+    secret = elem(parse_cloudinary_url(), 1)
 
     queryparams =
       additional_params
@@ -60,7 +61,14 @@ defmodule EmployeesApi.Cloudinary do
     |> Map.merge(%{
       "timestamp" => Integer.to_string(timestamp),
       "signature" => signature,
-      "api_key" => System.fetch_env!("CLOUDINARY_API_KEY")
+      "api_key" => elem(parse_cloudinary_url(), 0)
     })
+  end
+
+  defp parse_cloudinary_url() do
+    %{host: cloud_name, userinfo: userinfo} = @cloudinary_url
+    [api_key, api_secret] = String.split(userinfo, ":")
+
+    {api_key, api_secret, cloud_name}
   end
 end
